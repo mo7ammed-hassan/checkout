a38fbec2ae58c4168004c2cd4b39214f16f768fd
mohamedhassan <mohamed7github@gmail.com>
2024-09-05 12:00:40 +0300

    add styles file

---
+++ lib/core/utils/app_style.dart/styles.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const TextStyle style25 = TextStyle(
+    fontSize: 25,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle style18 = TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle style24 = TextStyle(
+    fontSize: 24,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle style18Bold = TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle style20 = TextStyle(
+    fontSize: 20,
+    color: Colors.black.withOpacity(0.80000001190929),
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle style22 = const TextStyle(
+    fontSize: 22,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle style16 = TextStyle(
+    fontSize: 16,
+    color: Colors.black54,
+    fontWeight: FontWeight.w400,
+  );
+}
